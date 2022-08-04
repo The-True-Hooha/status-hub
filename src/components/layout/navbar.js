4815e57fd1e1668a44a118940aac68aebb8ca6c1
David Ogar <owogogahhero@outlook.com>
2022-08-04 05:23:00 +0100

    refactor: side nav button fixed

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
-import { AiOutlineClose } from 'react-icons/ai'
 import { BiSearch, BiMenu } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { MdHome, MdNotifications, MdAddCircleOutline } from "react-icons/md";
 import { CgMenuGridR } from "react-icons/cg";
 
 export default function Navbar() {
-
-    //handles the side navbar for large and smaller screens
-    const [navbar, setNavbar] = useState(false);
-    const navMenu = () => {
-      setNavbar(!navbar);
-    };
-
+  //handles the side navbar for large and smaller screens
+  const [navbar, setNavbar] = useState(false);
+  const navMenu = () => {
+    setNavbar(!navbar);
+  };
 
   return (
     <header>
@@ -29,12 +26,62 @@ export default function Navbar() {
         </div>
         {/* handles the side nav bar*/}
         <div className="left-0 fixed top-0 md:w-[210px] z-30 w-full h-screen md:border md:border-r">
-            <div onClick={navMenu} className="flex justify-between w-full items-center px-10 mt-1.5">
-              <p className="text-[30px] font-bold font-mono text-blue-900">
-                hub
-              </p>
-            <BiMenu size={30} className="md:hidden"></BiMenu> 
-              
+          <div
+            onClick={navMenu}
+            className="flex justify-between w-full items-center px-10 mt-1.5"
+          >
+            <p className="text-[30px] font-bold font-mono text-blue-900">hub</p>
+            <BiMenu size={30} className="md:hidden"></BiMenu>
+          </div>
+          {/* starts here */}
+          <div
+            className={
+              navbar ? "md:grid grid-cols-1 pt-4 w-[60%] text-[15px]" : "hidden"
+            }
+          >
+            
+              <div className="pt-2 flex h-full items-center">
+                <MdHome size={20} className="text-gray-700 ml-4" />
+                <Link className="ml-4" to="/">
+                  {" "}
+                  My Apps
+                </Link>
+              </div>
+              <div className="pt-2">
+                <Link className="ml-[54px]" to="/">
+                  Flagship Pioneering
+                </Link>
+              </div>
+              <div className="pt-2">
+                <Link className="ml-[54px]" to="/">
+                  Subscriptions
+                </Link>
+              </div>
+              <div className="pt-2 flex h-full items-center">
+                <Link className="ml-[54px]" to="/">
+                  Add section
+                </Link>
+                <MdAddCircleOutline className="ml-1" />
+              </div>
+              <div className="pt-2 flex h-full items-center">
+                <MdNotifications size={20} className="text-gray-700 ml-4" />
+                <Link className="ml-4" to="/">
+                  Notifications
+                </Link>
+              </div>
+              <div className="pt-2 flex items-center h-full">
+                <CgMenuGridR size={20} className="ml-4" />
+                <Link className="ml-4" to="/">
+                  Add apps
+                </Link>
+              </div>
+            </div>
+            <div className="bottom-0 hidden left-0 absolute w-full text-[12px] font-bold pb-4">
+              <p className="flex justify-center">&copy; 2022 hub, inc.</p>
+              <Link className="flex justify-center" to="/">
+                privacy
+              </Link>
+            
           </div>
         </div>
       </div>
